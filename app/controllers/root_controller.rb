ab18df3de417e648a72678aa65f77105c8c40dfc
ghost <78723252+DmitriyFirsov@users.noreply.github.com>
2022-07-27 17:13:46 +0300

    articles list

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class RootController < ApplicationController
-  def index; end
+  def index
+    @articles = Article.eager_load(:user).all if user_signed_in?
+  end
 end

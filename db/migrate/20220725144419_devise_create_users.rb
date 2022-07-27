6627e24974958c27156bd858760b2ceb2fc53105
ghost <78723252+DmitriyFirsov@users.noreply.github.com>
2022-07-27 18:10:07 +0300

    fix

--- db/migrate/20220725144419_devise_create_users.rb
+++ db/migrate/20220725144419_devise_create_users.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 class DeviseCreateUsers < ActiveRecord::Migration[6.1]
+  def add_indexes
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+  end
+
+  def additional_fields
+    change_table :users do |t|
+      t.string :name, null: false
+      t.string :surname, null: false
+    end
+  end
+
   def change
     create_table :users do |t|
       ## Database authenticatable
@@ -13,14 +25,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
 
       ## Rememberable
       t.datetime :remember_created_at
-
       t.timestamps null: false
-
-      t.string :name, null: false
-      t.string :surname, null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    additional_fields
+    add_indexes
   end
 end

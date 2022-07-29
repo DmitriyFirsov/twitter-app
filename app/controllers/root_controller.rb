02b6482e50692558fb8dc9b58fb1efd63c04be65
DmitriyFirsov <78723252+DmitriyFirsov@users.noreply.github.com>
2022-07-29 10:28:25 +0300

    Update app/controllers/root_controller.rb
    Co-authored-by: Dmitry Barskov <dmitry.barskov@flatstack.com>

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,6 +2,6 @@
 
 class RootController < ApplicationController
   def index
-    @articles = Article.eager_load(:user).reorder(created_at: :DESC).all if user_signed_in?
+    @articles = Article.eager_load(:user).reorder(created_at: :desc).all if user_signed_in?
   end
 end

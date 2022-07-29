ca2544414784a4c985a76e355a3e0806b9e59185
ghost <78723252+DmitriyFirsov@users.noreply.github.com>
2022-07-29 18:55:55 +0300

    added registration test

---
+++ test/controllers/users/registrations_controller_test.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Users
+  class RegistrationsControllerTest < ActionDispatch::IntegrationTest
+    test "should permit additional fields to modal" do
+      user = {
+        name: "name",
+        surname: "surname",
+        email: "test@example.com",
+        password: "password",
+        password_confirmation: "password"
+      }
+
+      post "/user", params: {
+        user: user
+      }
+
+      user_list = User.find_by!(name: user[:name], surname: user[:surname], email: user[:email])
+      assert user_list
+    end
+  end
+end

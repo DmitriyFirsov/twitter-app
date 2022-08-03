3fec407c976a5d2b4df5d9d6bc6d3a8f814bd06e
DmitriyFirsov <78723252+DmitriyFirsov@users.noreply.github.com>
2022-08-03 13:45:16 +0300

    Fix permit additional fields for regisration  (#5)
    Co-authored-by: Dmitry Barskov <dmitry.barskov@flatstack.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :user
+  devise_for :user, controllers: { registrations: "users/registrations" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "root#index"
 end
